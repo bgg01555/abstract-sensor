3b835ce1c3070a0a077213d85791cf71f2e553f7
jathazp <bgg01580@gmail.com>
2022-02-07 22:43:14 +0900

    서버에서 표준 입력 출력 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const { Server } = require('http');
+const { addListener } = require('process');
+
 class Sensor {
     constructor(id) {
         this.id = id;
@@ -14,6 +17,7 @@ class Sensor {
 
     data_reporting() {
         this.status = "reportingData";
+        IotServer.std_output(this.status);
         this.running_settimeout_Id = setTimeout(() => this.switch_idle(), 1000);
     }
 
@@ -44,6 +48,13 @@ class Sensor {
 class IotServer {
     constructor() {
         this.running_sensors = [];
+
+        process.on('CHANGE_REPORTING_INTERVAL', function (deviceId, interval, that) {
+            let sensor = that.running_sensors.find(x => x.id == deviceId);
+            if (sensor.powerStatus === "on" && interval >= 0 && interval <= 10000) {
+                sensor.reportingInterval = interval;
+            }
+        })
     }
 
     start(sensor) {
@@ -53,12 +64,44 @@ class IotServer {
     publish(eventObj) {
         let sensor = this.running_sensors.find(x => x.id === eventObj.deviceId);
         if (sensor.powerStatus === "on") {
-            if (eventObj.actionId === 'CHANGE_REPORTING_INTERVAL') {
-                sensor.reportingInterval = eventObj.payload;
-            }
+            process.emit(eventObj.actionId, eventObj.deviceId, eventObj.payload, this);
+            // if (eventObj.actionId === 'CHANGE_REPORTING_INTERVAL') {
+            //     sensor.reportingInterval = eventObj.payload;
+            // }
         }
     }
+
+    static std_output(reportingData) {
+        console.log(reportingData);
+    }
+
+    std_input() {
+        let readlineSync = require('readline-sync');
+        let deviceId = readlineSync.questionInt('id:');
+        let interval = readlineSync.questionInt('interval:');
+        //if (!isNaN(deviceId) && !isNaN(interval)) {
+        deviceId = `id${deviceId}`;
+        this.publish({
+            deviceId: deviceId,
+            actionId: 'CHANGE_REPORTING_INTERVAL',
+            payload: interval
+        });
+        //this.user_input();
+        // this.publish({
+        //     deviceId: `id${deviceId}`,
+        //     actionId: 'CHANGE_REPORTING_INTERVAL',
+        //     payload: interval,
+        // })
+        //}
+    };
 }
+// const sensor = new Sensor('id1');
+
+// sensor.turn('on');
+
+// const server = new IotServer();
+// server.start([sensor]);
+// server.std_input();
 
 module.exports = {
     Sensor,
